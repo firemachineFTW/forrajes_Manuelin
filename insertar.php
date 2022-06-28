c443efc6d56ae1fe6b80ce1bcad12cbf1098c6a0
JorgeATM <61215359+JorgeATM@users.noreply.github.com>
2022-06-28 12:39:26 -0500

    Update insertar.php

--- insertar.php
+++ insertar.php
@@ -33,7 +33,7 @@
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.php">Venta</a></li>
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="insertar.php">Insertar</a></li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Compra</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="compra.php">Compra</a></li>
                 </ul>
             </div>
         </div>
