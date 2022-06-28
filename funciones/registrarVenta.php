cbf428c0ac1f870d2d17c6a1eabec1bb93256f6c
firemachineFTW <61358182+firemachineFTW@users.noreply.github.com>
2022-06-28 11:20:02 -0500

    juanjo2

--- funciones/registrarVenta.php
+++ funciones/registrarVenta.php
@@ -4,15 +4,32 @@
     $idVenta = $_POST['txtFolio'];
     $FechaHoraVenta = $_POST['txtFecha'];
     $TotalVenta = $_POST['txtTotal'];
+    $idProductoVenta = $_POST['txtCodigoProducto'];
+    $Cantidad = $_POST['txtCantidad'];
+    $PrecioVenta = $_POST['txtPrecioVenta'];
+    $idProducto = $_POST['cmbProductoVenta'];
 
     $sentencia = "INSERT INTO venta VALUES(
         '$idVenta',
         '$FechaHoraVenta',
-        '$TotalVenta');"
+        '$TotalVenta')";
 
-     echo 
-     (mysqli_query($conexion, $sentencia)){
-        header("Location: ../index.html");
+    
+        
+    if(mysqli_query($conexion, $sentencia)){
+        $sentencia1 = "INSERT INTO productoventa VALUES(
+            DEFAULT,
+            '$idVenta',
+            '$Cantidad',
+            '$PrecioVenta',
+            '$idProducto')";
+
+            if(mysqli_query($conexion, $sentencia1)){
+                header("Location: ../index.html");
+            }else{
+                echo "no";
+            }
+        
     }else{
         echo "no";
     }
