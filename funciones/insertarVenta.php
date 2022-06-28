af6c12153c21f387125bd63fff5fb17895c5a0a3
firemachineFTW <61358182+firemachineFTW@users.noreply.github.com>
2022-06-28 14:56:10 -0500

    juan4

--- funciones/insertarVenta.php
+++ funciones/insertarVenta.php
@@ -1,23 +1,23 @@
 <?php
+
     include("../conexiones/conexion.php");
 
-    $FechaHoraVenta = $_POST['dtFecha'];
     $Cantidad = $_POST['txtCantidad'];
     $PrecioVenta = $_POST['txtPrecioVenta'];
+    $FechaHoraVenta = $_POST['dtFecha'];
     $idProducto = $_POST['cmbProductoVenta'];
 
-    
-        $sentencia = "INSERT INTO productoventa VALUES(
-             DEFAULT,
-            '$Cantidad',
-            '$PrecioVenta',
-            '$FechaHoraVenta',
-            '$idProducto')";
+    $sentencia = "INSERT INTO productoventa VALUES(
+         DEFAULT,
+        '$Cantidad',
+        '$PrecioVenta',
+        '$FechaHoraVenta',
+        '$idProducto')";
 
-            if(mysqli_query($conexion,$sentencia)){
-                header("Location: ../about.php");
-            }else{
-                echo "no";
-            }
+    if(mysqli_query($conexion,$sentencia)){
+        header("Location: ../index.html");
+    }else{
+        echo "no";
+    }
 
     mysqli_close($conexion);
